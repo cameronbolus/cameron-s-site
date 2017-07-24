043aeca658c812fea59299b6cdec10be01e37a52
Cameron Bolus <cameronbolus@Camerons-MacBook-Pro.local>
2017-07-24 16:41:36 -0400

    update portfolio and resume

--- src-client/scripts/components/navComp.js
+++ src-client/scripts/components/navComp.js
@@ -27,7 +27,7 @@ export const NavComponent = React.createClass({
         <img className="logo" src="../images/cameron-name-red.png" alt=""></img>
         <div className="nav-options">
 					<div className="hover-color spaced-out" onClick={this._handleNavigationClick} data-route="HOME">Home</div>
-          <div className="resume-link hover-color spaced-out"><a className="resume-link hover-color spaced-out" href="../images/CBDevResume.pdf" target="_blank">Resume</a></div>
+          <div className="resume-link hover-color spaced-out"><a className="resume-link hover-color spaced-out" href="../images/newDevResume.pdf" target="_blank">Resume</a></div>
           <div className="hover-color spaced-out" onClick={this._handleNavigationClick} data-route="PORTFOLIO">Portfolio</div>
         </div>
 			</nav>
